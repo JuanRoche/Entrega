1274f7709c630dd24b40d5e4a8248e671b82483a
Juan Andres Roche <rochejuanandres@gmail.com>
2024-03-19 09:37:27 -0300

    cambio de numero de intentos por numero de fallos

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+failures = 0
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 
@@ -17,7 +17,7 @@ word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
 
-for i in range(max_attempts):
+while(failures < 3):
      # Pedir al jugador que ingrese una letra
      letter = input("Ingresa una letra: ").lower()
      # Verificar si la letra ya ha sido adivinada
@@ -31,8 +31,11 @@ for i in range(max_attempts):
          # Soluciona que no sea caracter vacio 
          if(letter != ""):
             print("¡Bien hecho! La letra está en la palabra.")
-         else:
-            print("Lo siento, la letra no está en la palabra.")
+         else: 
+            print("Caracter no válido, ingrese otro:")
+     else:
+        print("Lo siento, la letra no está en la palabra.")
+        failures += 1
      # Mostrar la palabra parcialmente adivinada
      letters = []
      for letter in secret_word:
@@ -47,5 +50,5 @@ for i in range(max_attempts):
          print(f"¡Felicidades! Has adivinado la palabra secreta:  {secret_word}")
          break
 else:
-     print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
-     print(f"La palabra secreta era: {secret_word}")
+     print(f"¡Oh no! alcanzaste el limite de fallos")
+     print(f"La palabra secreta era: {secret_word}")    
